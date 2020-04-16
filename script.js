cb9b5180fcc2e230e3881aea0e97b0c960a64624
Zarina M <zarinabliss@gmail.com>
2020-04-16 16:48:50 -0400

    created new function to display results

--- script.js
+++ script.js
@@ -2,6 +2,32 @@
 let restaurantsApp = {}
 //Store API key
 restaurantsApp.apiKey = '2-RCWO0-I-9m7PMN2zt0fcZ45itXKXWRfnBQtimCYUjh2skNC9-_CAF_SJdwlTkeymvzhlzSyQFDz0kih-S3Cjz1JIxklzgXrnO-YySwD4ThKeBFlskagdf0JeeVXnYx';
+
+restaurantsApp.displayRestaurantDetails = function(result) {
+    //Empty restaurantList so we can append results each time
+    $('.restaurantList').empty()
+    //This takes the first three results, can be changed later
+    for (let i = 0; i < 3; i++) {
+        let businessID = result.businesses[i].id
+        let businessName = result.businesses[i].name
+        let businessImage = result.businesses[i].image_url
+        let businessRating = result.businesses[i].rating
+        let businessPrice = result.businesses[i].price
+        let businessAddress = result.businesses[i].location.display_address
+
+        let html = `<div>
+                <img src="${businessImage}">
+                <h2>${businessName}</h2>
+                <span>${businessRating}</span>
+                <span><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i></span> 
+                <h3>${businessAddress}</h3>
+                </div>`
+        //Displays each result to the page
+        $('.restaurantList').append(html)
+    }
+}
+
+
 //Retrieve restaurant ID's 
 restaurantsApp.getRestaurantIDs = (searchTerm, searchLocation) => {
     $.ajax({
@@ -19,27 +45,7 @@ restaurantsApp.getRestaurantIDs = (searchTerm, searchLocation) => {
             location: searchLocation
         }
     }).then(function (result) {
-        //Empty restaurantList so we can append results each time
-        $('.restaurantList').empty()
-        //This takes the first three results, can be changed later
-        for (let i = 0; i < 3; i++) {
-            let businessID = result.businesses[i].id
-            let businessName = result.businesses[i].name
-            let businessImage = result.businesses[i].image_url
-            let businessRating = result.businesses[i].rating
-            let businessPrice = result.businesses[i].price
-            let businessAddress = result.businesses[i].location.display_address
-
-            let html = `<div>
-                <img src="${businessImage}">
-                <h2>${businessName}</h2>
-                <span>${businessRating}</span>
-                <span><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i></span> 
-                <h3>${businessAddress}</h3>
-                </div>`
-            //Displays each result to the page
-            $('.restaurantList').append(html)
-        }
+        restaurantsApp.displayRestaurantDetails(result)        
     });
 
 }
