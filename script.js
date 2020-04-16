6e4f2571552a38178352ed71414f0f0a9143c8a3
mahsa Ashtari <mahsa_91@live.com>
2020-04-16 13:02:31 -0400

    second ajax call added

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 let restaurantsApp = {}
 //Store API key
 restaurantsApp.apiKey = '2-RCWO0-I-9m7PMN2zt0fcZ45itXKXWRfnBQtimCYUjh2skNC9-_CAF_SJdwlTkeymvzhlzSyQFDz0kih-S3Cjz1JIxklzgXrnO-YySwD4ThKeBFlskagdf0JeeVXnYx';
+// Create empty arrays to store businesses information
+restaurantsApp.businessID=[]
+restaurantsApp.businessName=[]
+restaurantsApp.businessAddress=[]
+restaurantsApp.businessRating=[]
+restaurantsApp.businessPrice=[]
+restaurantsApp.businessReviews=[]
 //Retrieve restaurant ID's 
 restaurantsApp.getRestaurantIDs = (searchTerm, searchLocation) => {
     $.ajax({
@@ -16,26 +23,55 @@ restaurantsApp.getRestaurantIDs = (searchTerm, searchLocation) => {
         },
         data: {
             term: searchTerm,
-            location: searchLocation
+            location: searchLocation,
+            limit: 3,
         }
     }).then(function (result) {
-        //This takes the first three results, can be changed later
-        for (let i = 0; i < 3; i++) {
-            let businessID = result.businesses[i].id
-            let businessName = result.businesses[i].name
-            let businessRating = result.businesses[i].rating
-            let businessPrice = result.businesses[i].price
-            let businessAddress = result.businesses[i].location.display_address
-        }
-    });
+        console.log(result)
+        // result.bussinesses is an array on bussinesses that can be looped through with forEach method ,then for each business object the info are stored in respective array
+        result.businesses.forEach(function(item){
+
+             restaurantsApp.businessID.push(item.id)
+             restaurantsApp.businessRating.push(item.rating)
+             restaurantsApp.businessPrice.push(item.price)
+             restaurantsApp.businessName.push(item.name)
+             restaurantsApp.businessAddress.push(item.location.display_address) 
+          
+    }); 
+    restaurantsApp.getReviews()
+})
 
 }
+restaurantsApp.getReviews=function(){
+    restaurantsApp.businessID.forEach(function(item){
+        console.log(item)
+        $.ajax({
+            url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${item}/reviews`,
+            method: "GET",
+            headers: {
+            "accept": "application/json",
+            "x-requested-with": "xmlhttprequest",
+            "Access-Control-Allow-Origin": "*",
+            "Authorization": `Bearer ${restaurantsApp.apiKey}`
+            },
+            data: {}
+        }).then(function(result){
+            restaurantsApp.businessReviews.push(result.reviews)
+            console.log(restaurantsApp.businessReviews)
+        })
+        .catch(function(result){
+            console.log(result)
+
+        })
+        
+    })
+}
+
 restaurantsApp.init = function () {
     restaurantsApp.getRestaurantIDs('Chicken', 'Toronto')
-
 }
 
 
 $(function() {
     restaurantsApp.init()
-});
+})
