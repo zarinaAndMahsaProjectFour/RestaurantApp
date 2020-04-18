910b06ed0f345634027128b607ae96f6e903a9f0
mahsa Ashtari <mahsa_91@live.com>
2020-04-18 19:46:14 -0400

    buttons added to show more results

--- script.js
+++ script.js
@@ -3,62 +3,6 @@ let restaurantsApp = {}
 //Store API key
 restaurantsApp.apiKey = '2-RCWO0-I-9m7PMN2zt0fcZ45itXKXWRfnBQtimCYUjh2skNC9-_CAF_SJdwlTkeymvzhlzSyQFDz0kih-S3Cjz1JIxklzgXrnO-YySwD4ThKeBFlskagdf0JeeVXnYx';
 
-restaurantsApp.displayRestaurantDetails = function(result) {
-    
-        $('.restaurantListOne').empty()
-        //This takes the first three results, can be changed later
-        for (let i = 0; i < 6; i++) {
-            let businessID = result.businesses[i].id
-            let businessName = result.businesses[i].name
-            let businessImage = result.businesses[i].image_url
-            let businessRating = result.businesses[i].rating
-            let businessPrice = result.businesses[i].price
-            let businessAddress = result.businesses[i].location.display_address
-    
-            let html = `<div>
-                    <img src="${businessImage}">
-                    <h2>${businessName}</h2>
-                    <span>${businessRating}</span>
-                    <span><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i></span> 
-                    <h3>${businessAddress}</h3>
-                    </div>`
-            //Displays each result to the page
-            $('.listOne').append(html)
-        }
-
-        $('.restaurantListTwo').empty()
-        //This takes the first three results, can be changed later
-        for (let i = 6; i < 12; i++) {
-            let businessID = result.businesses[i].id
-            let businessName = result.businesses[i].name
-            let businessImage = result.businesses[i].image_url
-            let businessRating = result.businesses[i].rating
-            let businessPrice = result.businesses[i].price
-            let businessAddress = result.businesses[i].location.display_address
-
-            let html = `<div>
-                    <img src="${businessImage}">
-                    <h2>${businessName}</h2>
-                    <span>${businessRating}</span>
-                    <span><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i></span> 
-                    <h3>${businessAddress}</h3>
-                    </div>`
-            //Displays each result to the page
-            $('.listTwo').append(html)
-        }
-}
-
-restaurantsApp.displayMore=function(){
-    $('.restaurantList p').on('click', function(){
-        $('.listOne').removeClass('unhidden')
-        $('.listOne').addClass('hidden')
-        $('.listTwo').removeClass('hidden')
-        $('.listTwo').addClass('unhidden')
-    })
-}
-
-
-
 //Retrieve restaurant Info
 restaurantsApp.getRestaurants = (searchTerm, searchLocation) => {
     
@@ -108,9 +52,80 @@ restaurantsApp.getReviews=function(result){
         })
     }}
 
+restaurantsApp.displayRestaurantDetails = function(result) {
+  
+    $('.ListOne').empty()
+    //This takes the first three results, can be changed later
+    for (let i = 0; i < 6; i++) {
+        let businessID = result.businesses[i].id
+        let businessName = result.businesses[i].name
+        let businessImage = result.businesses[i].image_url
+        let businessRating = result.businesses[i].rating
+        let businessPrice = result.businesses[i].price
+        let businessAddress = result.businesses[i].location.display_address
+
+        let html = `<div>
+                <img src="${businessImage}">
+                <h2>${businessName}</h2>
+                <span>${businessRating}</span>
+                <span><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i></span> 
+                <h3>${businessAddress}</h3>
+                </div>`
+        //Displays each result to the page
+        $('.listOne').append(html)
+    }
+
+    $('.ListTwo').empty()
+    //This takes the first three results, can be changed later
+    for (let i = 6; i < 12; i++) {
+        let businessID = result.businesses[i].id
+        let businessName = result.businesses[i].name
+        let businessImage = result.businesses[i].image_url
+        let businessRating = result.businesses[i].rating
+        let businessPrice = result.businesses[i].price
+        let businessAddress = result.businesses[i].location.display_address
+
+        let html = `<div>
+                <img src="${businessImage}">
+                <h2>${businessName}</h2>
+                <span>${businessRating}</span>
+                <span><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i></span> 
+                <h3>${businessAddress}</h3>
+                </div>`
+        //Displays each result to the page
+        $('.listTwo').append(html)
+    } 
+        
+}
+
+restaurantsApp.showMore=function(){
+    
+    let slideIndex = 1;
+    showDivs(slideIndex);
+
+    function showDivs(n) {
+        let i;
+        let x = $(".restaurantList");
+        console.log(x)
+        if (n > x.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = x.length} ;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex-1].style.display = "grid";
+    }
+    
+    $('button.right').on('click', function(){
+        showDivs(slideIndex += +1)
+    })
+    $('button.left').on('click', function(){
+        showDivs(slideIndex += -1)
+    })
+
+}
 restaurantsApp.init = function () {
     restaurantsApp.getRestaurants('Chicken', 'northyork ontario')
-    restaurantsApp.displayMore()
+    restaurantsApp.showMore()
 }
 
 
